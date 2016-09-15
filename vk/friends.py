c89b7d601152af1d489d8ce0df90d38bfdec210f
Sergey G <s.gaynetdinov@gmail.com>
2016-09-16 00:10:21 +0500

    Fix

--- vk/friends.py
+++ vk/friends.py
@@ -7,7 +7,7 @@ class Friends(object):
     @staticmethod
     def get_friends(user_id):
         user_id_items = fetch('friends.get', user_id=user_id)
-        return get_users(*user_id_items)
+        return get_users(user_id_items)
 
     @staticmethod
     def get_friends_count(user_id):
