00bf496d42f254e58d5f81dc582fa9089e980b16
Sergey G <s.gaynetdinov@gmail.com>
2017-02-26 21:21:37 +0500

    New version `2017.2.26`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='py-vkontakte',
-    version='2016.11',
+    version='2017.2.26',
     packages=['vk'],
     url='https://github.com/sgaynetdinov/py-vkontakte',
     license='MIT License',
@@ -11,10 +11,18 @@ setup(
     author_email='s.gaynetdinov@gmail.com',
     description='Python API wrapper around vk.com API',
     long_description=open('README.rst').read(),
-    keywords='vk, vkontakte',
+    keywords='vk.com vk, vkontakte',
     classifiers=[
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=[
         'requests',
