8d02912d2c38aa75e8565eb92ca351c0d612767c
Sergey G <s.gaynetdinov@gmail.com>
2017-02-26 17:47:47 +0500

    Refactoring

--- vk/fetch.py
+++ vk/fetch.py
@@ -1,35 +1,9 @@
 # coding=utf-8
-import functools
 import urllib
 import requests
 
 from .error import VKError, VKParseJsonError
 from .auth import get_access_token
-
-
-def fetch_field(method_name, field_name=None):
-
-    def decorate(func):
-
-        @functools.wraps(func)
-        def f(self):
-            _field_name = field_name if field_name else func.__name__
-            try:
-                return func(self)
-            except KeyError:
-                users_json = fetch(method_name, user_ids=self.id, fields=_field_name)
-                user = self.__class__(users_json[0])
-                try:
-                    self._data.update({
-                        _field_name: user._data[_field_name]
-                    })
-                    return getattr(self, func.__name__)
-                except KeyError:
-                    raise AttributeError("object '{0}' has no attribute '{1}'".format(self.__class__.__name__, _field_name))
-
-        return f
-
-    return decorate
 
 
 def fetch(method_name, **params):
