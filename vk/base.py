3465b460bcfe2f8f7dad191de24ecc94f98ffc28
Sergey G <s.gaynetdinov@gmail.com>
2017-03-23 18:47:14 +0500

    Refactoring

--- vk/base.py
+++ vk/base.py
@@ -6,4 +6,4 @@ class VKObject(object):
             vkobject_title = self.screen_name
         else:
             vkobject_title = "id{0}".format(self.id)
-        return u"<{0} {1}>".format(self.__class__.__name__, vkobject_title)
+        return u"<{0}: {1}>".format(self.__class__.__name__, vkobject_title)
