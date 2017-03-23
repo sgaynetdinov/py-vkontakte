a3b5337cd15d23a58eb92aa922c82f49ff8b5fdd
Sergey G <s.gaynetdinov@gmail.com>
2017-03-23 22:35:22 +0500

    Sort alphabetically

--- vk/wall.py
+++ vk/wall.py
@@ -11,42 +11,43 @@ class Wall(VKObject):
     """
     Docs: https://vk.com/dev/objects/post
     """
-    __slots__ = ('id', 'owner_id', 'from_id', 'unixtime', 'date', 'text', 'reply_owner_id', 'reply_post_id', 'friends_only', 'comments_count',
-                 'likes_count', 'reposts_count', 'post_type', 'is_pinned', 'is_ads', 'attachments', 'signer_id')
+
+    __slots__ = ('attachments', 'comments_count', 'date', 'friends_only', 'from_id', 'id', 'is_ads', 'is_pinned', 'likes_count', 'owner_id',
+                 'post_type', 'reply_owner_id', 'reply_post_id', 'reposts_count', 'signer_id', 'text', 'unixtime')
 
     @classmethod
     def from_json(cls, wall_json):
         wall = cls()
-        wall.id = wall_json.get("id")
-        wall.owner_id = wall_json.get("owner_id")
-        wall.from_id = wall_json.get("from_id")
-        wall.unixtime = wall_json.get("date")
+        wall.attachments = get_attachments(wall_json.get("attachments"))
+        wall.comments_count = wall_json.get('comments')['count']
         wall.date = datetime.utcfromtimestamp(wall.unixtime)
-        wall.text = wall_json.get("text")
+        wall.friends_only = wall_json.get("friends_only")
+        wall.from_id = wall_json.get("from_id")
+        wall.id = wall_json.get("id")
+        wall.is_ads = bool(wall_json.get("marked_as_ads"))
+        wall.is_pinned = bool(wall_json.get("is_pinned"))
+        wall.likes_count = wall_json.get('likes')['count']
+        wall.owner_id = wall_json.get("owner_id")
+        wall.post_type = wall_json.get("post_type")
         wall.reply_owner_id = wall_json.get("reply_owner_id")
         wall.reply_post_id = wall_json.get("reply_post_id")
-        wall.friends_only = wall_json.get("friends_only")
-        wall.comments_count = wall_json.get('comments')['count']
-        wall.likes_count = wall_json.get('likes')['count']
         wall.reposts_count = wall_json.get('reposts')['count']
-        wall.post_type = wall_json.get("post_type")
-        wall.is_pinned = bool(wall_json.get("is_pinned"))
-        wall.is_ads = bool(wall_json.get("marked_as_ads"))
-        wall.attachments = get_attachments(wall_json.get("attachments"))
         wall.signer_id = wall_json.get("signer_id")
+        wall.text = wall_json.get("text")
+        wall.unixtime = wall_json.get("date")
         return wall
 
     @classmethod
     def from_json_items(cls, wall_json_items):
         return (cls.from_json(wall_json) for wall_json in wall_json_items)
 
-    def pin(self):
-        response = fetch("wall.pin", owner_id=self.owner_id, post_id=self.id)
-        return bool(response)
+    @property
+    def copy_history(self):
+        raise NotImplementedError
 
-    def unpin(self):
-        response = fetch("wall.unpin", owner_id=self.owner_id, post_id=self.id)
-        return bool(response)
+    @property
+    def geo(self):
+        raise NotImplementedError
 
     def get_comments(self):
         return Comment.get_comments(group_or_user_id=self.owner_id, wall_id=self.id)
@@ -54,20 +55,8 @@ class Wall(VKObject):
     def get_comments_count(self):
         return Comment.get_comments_count(group_or_user_id=self.owner_id, wall_id=self.id)
 
-    @property
-    def geo(self):
-        raise NotImplementedError
-
-    @property
-    def copy_history(self):
-        raise NotImplementedError
-
     def get_url(self):
         return 'https://vk.com/wall{0}_{1}'.format(self.owner_id, self.id)
-
-    @staticmethod
-    def get_walls(owner_id):
-        return fetch_items("wall.get", Wall.from_json_items, 100, owner_id=owner_id)
 
     @staticmethod
     def get_wall(owner_id, wall_id):
@@ -78,7 +67,19 @@ class Wall(VKObject):
         return Wall.from_json(response[0])
 
     @staticmethod
+    def get_walls(owner_id):
+        return fetch_items("wall.get", Wall.from_json_items, 100, owner_id=owner_id)
+
+    @staticmethod
     def get_wall_count(owner_id):
         response = fetch("wall.get", owner_id=owner_id, count=1)
         wall_count = response.get('count')
         return wall_count
+
+    def pin(self):
+        response = fetch("wall.pin", owner_id=self.owner_id, post_id=self.id)
+        return bool(response)
+
+    def unpin(self):
+        response = fetch("wall.unpin", owner_id=self.owner_id, post_id=self.id)
+        return bool(response)
