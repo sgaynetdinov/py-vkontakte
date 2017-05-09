922d7121aa8a1618e0909e54b109e794671a7cc7
Sergey Gaynetdinov <s.gaynetdinov@gmail.com>
2017-05-09 17:16:59 +0500

    Rename `create_url_get_code` => `get_url_authcode_flow_user`
    Add docs

--- vk/auth.py
+++ vk/auth.py
@@ -8,7 +8,7 @@ import requests
 
 from .error import VKError
 
-__all__ = ["set_access_token", "get_access_token", "get_url_implicit_flow_user", "create_access_token_from_code", "create_url_get_code"]
+__all__ = ["set_access_token", "get_access_token", "get_url_implicit_flow_user", "create_access_token_from_code", "get_url_authcode_flow_user"]
 
 _ACCESS_TOKEN = None
 
@@ -46,20 +46,43 @@ def get_url_implicit_flow_user(client_id, scope,
     return u"{url}?{params}".format(url=url, params=urlencode(params))
 
 
-def create_url_get_code(client_id, redirect_uri, display="page", scope=None, response_type="code", version=None, state=None):
+def get_url_authcode_flow_user(client_id, redirect_uri, display="page", scope=None, state=None):
+    """Authorization Code Flow for User Access Token
+    
+    Use Authorization Code Flow to run VK API methods from the server side of an application.
+    Access token received this way is not bound to an ip address but set of permissions that can be granted is limited for security reasons.
+    
+    Args:
+        client_id (int): Application id. 
+        redirect_uri (str): Address to redirect user after authorization. 
+        display (str): Sets authorization page appearance.
+            Sets: {`page`, `popup`, `mobile`}
+            Defaults to `page`
+        scope (:obj:`str`, optional): Permissions bit mask, to check on authorization and request if necessary.
+            More scope: https://vk.com/dev/permissions 
+        state (:obj:`str`, optional): An arbitrary string that will be returned together with authorization result. 
+
+    Returns:
+        str: Url
+    
+    Examples:
+        >>> vk.get_url_authcode_flow_user(1, 'http://example.com/', scope="wall,email")
+        'https://oauth.vk.com/authorize?client_id=1&display=page&redirect_uri=http://example.com/&scope=wall,email&response_type=code
+        
+    .. _Docs:
+        https://vk.com/dev/authcode_flow_user
+
+    """
     url = "https://oauth.vk.com/authorize"
     params = {
         "client_id": client_id,
         "redirect_uri": redirect_uri,
         "display": display,
-        "response_type": response_type
+        "response_type": "code"
     }
 
     if scope:
         params['scope'] = scope
-
-    if version:
-        params['v'] = version
 
     if state:
         params['state'] = state
