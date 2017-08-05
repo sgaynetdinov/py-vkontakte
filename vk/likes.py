06e31815a55892b11c8673cea10ea9d0779e5fa6
Sergey G <s.gaynetdinov@gmail.com>
2017-08-06 01:38:38 +0500

    Not used

--- vk/likes.py
+++ vk/likes.py
@@ -1,10 +1,9 @@
 # coding=utf-8
-from .fetch import fetch_items
 from .users import get_users
 
 
-def get_likes(comment_type, group_or_user_id, comment_id):
-    """
-    https://vk.com/dev/likes.getList
-    """
-    return fetch_items('likes.getList', get_users, count=100, type=comment_type, owner_id=group_or_user_id, item_id=comment_id)
+# def get_likes(comment_type, group_or_user_id, comment_id):
+#     """
+#     https://vk.com/dev/likes.getList
+#     """
+#     return fetch_items('likes.getList', get_users, count=100, type=comment_type, owner_id=group_or_user_id, item_id=comment_id)
