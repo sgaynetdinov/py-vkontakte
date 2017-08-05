e01a4c931d8bcd95b4f214e622e201d11e7bcf68
Sergey G <s.gaynetdinov@gmail.com>
2017-08-05 14:52:10 +0500

    Added a link to the documentation

--- vk/friends.py
+++ vk/friends.py
@@ -6,11 +6,17 @@ from .users import get_users
 class Friends(object):
     @staticmethod
     def get_friends(user_id):
+        """
+        https://vk.com/dev/friends.get
+        """
         response = fetch('friends.get', user_id=user_id)
         user_id_items = response["items"]
         return get_users(user_id_items)
 
     @staticmethod
     def get_friends_count(user_id):
+        """
+        https://vk.com/dev/friends.get
+        """
         response = fetch('friends.get', user_id=user_id, count=1)
         return response["count"]
