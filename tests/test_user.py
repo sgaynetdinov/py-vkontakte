4ed51da41de95b2cd7bedccb2dc55dd19d46bf31
Sergey G <s.gaynetdinov@gmail.com>
2017-09-23 23:38:12 +0500

    Add test `test_return_None_when_unixtime_is_None`

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,7 +7,12 @@ class UserTestCase(unittest.TestCase):
 
     def setUp(self):
         self.unixtime = {'time': 1506187303}
+        self.unixtime_is_none = None
 
     def test_fail_if_not_convert_unixtime_to_datetime_UTC(self):
         last_seen_in_datetime = User._last_seen(self.unixtime)
         self.assertEqual(last_seen_in_datetime, datetime.datetime(2017, 9, 23, 17, 21, 43))
+
+    def test_return_None_when_unixtime_is_None(self):
+        last_seen_in_datetime = User._last_seen(self.unixtime_is_none)
+        self.assertIsNone(last_seen_in_datetime)
