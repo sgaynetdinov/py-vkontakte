0ee6b30823800d81cc499b49f753b5d18ea016a8
Sergey G <sergey@gaynetdinov.ru>
2018-09-07 23:14:52 +0500

    Delete support `python 2.7`

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
