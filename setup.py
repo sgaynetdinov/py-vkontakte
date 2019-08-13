0b24f8048fc7bf5a3fad1d0cf80c71ceaf29a740
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-08-14 02:34:38 +0500

    version '5.66' -> '5.69'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='py-vkontakte',
-    version='5.66.0',
+    version='5.69.0',
     packages=['vk'],
     url='https://github.com/sgaynetdinov/py-vkontakte',
     license='MIT License',
