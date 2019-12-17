4215434119b57c389cc561432a99ca2dde0cb0a4
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-12-17 16:51:28 +0500

    Delete, not used

--- vk/likes.py
+++
@@ -1,7 +0,0 @@
-from .users import get_users
-
-# def get_likes(comment_type, group_or_user_id, comment_id):
-#     """
-#     https://vk.com/dev/likes.getList
-#     """
-#     return fetch_items('likes.getList', get_users, count=100, type=comment_type, owner_id=group_or_user_id, item_id=comment_id)
