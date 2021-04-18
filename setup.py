414269b74598f6e718c0a999f68be5931bf158c1
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2021-04-19 00:42:24 +0500

    Set version '5.70.2'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='py-vkontakte',
-    version='5.70.1',
+    version='5.70.2',
     packages=['vk'],
     url='https://github.com/sgaynetdinov/py-vkontakte',
     license='MIT License',
